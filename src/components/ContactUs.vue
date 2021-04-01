35d13632d4cda0b8dccfbd17a4dc23584563abbf
Kimberly Yen <work223305@gmail.com>
2021-04-01 18:11:34 +0800

    edit

--- src/components/ContactUs.vue
+++ src/components/ContactUs.vue
@@ -25,6 +25,54 @@
             <dd>
               <p class="mod-link-text"><a href="/csr/fair_operating_practices/compliance_required_pharmaceuticals/index.html">「企業活動と医療機関等の関係の透明性に関する指針」ならびに「企業活動と患者団体の関係の透明性に関する指針」に基づいた2019年の企業活動情報を公開しました</a></p>
             </dd>
+          </div> 
+          <div class="list">
+            <dt>2020年9月11日</dt>
+            <dd>
+              <p class="mod-link-text"><a href="/csr/fair_operating_practices/compliance_required_pharmaceuticals/index.html">「企業活動と医療機関等の関係の透明性に関する指針」ならびに「企業活動と患者団体の関係の透明性に関する指針」に基づいた2019年の企業活動情報を公開しました</a></p>
+            </dd>
+          </div>
+          <div class="list">
+            <dt>2020年9月11日</dt>
+            <dd>
+              <p class="mod-link-text"><a href="/csr/fair_operating_practices/compliance_required_pharmaceuticals/index.html">「企業活動と医療機関等の関係の透明性に関する指針」ならびに「企業活動と患者団体の関係の透明性に関する指針」に基づいた2019年の企業活動情報を公開しました</a></p>
+            </dd>
+          </div>
+          <div class="list">
+            <dt>2020年9月11日</dt>
+            <dd>
+              <p class="mod-link-text"><a href="/csr/fair_operating_practices/compliance_required_pharmaceuticals/index.html">「企業活動と医療機関等の関係の透明性に関する指針」ならびに「企業活動と患者団体の関係の透明性に関する指針」に基づいた2019年の企業活動情報を公開しました</a></p>
+            </dd>
+          </div>
+          <div class="list">
+            <dt>2020年9月11日</dt>
+            <dd>
+              <p class="mod-link-text"><a href="/csr/fair_operating_practices/compliance_required_pharmaceuticals/index.html">「企業活動と医療機関等の関係の透明性に関する指針」ならびに「企業活動と患者団体の関係の透明性に関する指針」に基づいた2019年の企業活動情報を公開しました</a></p>
+            </dd>
+          </div>
+          <div class="list">
+            <dt>2020年9月11日</dt>
+            <dd>
+              <p class="mod-link-text"><a href="/csr/fair_operating_practices/compliance_required_pharmaceuticals/index.html">「企業活動と医療機関等の関係の透明性に関する指針」ならびに「企業活動と患者団体の関係の透明性に関する指針」に基づいた2019年の企業活動情報を公開しました</a></p>
+            </dd>
+          </div>
+          <div class="list">
+            <dt>2020年9月11日</dt>
+            <dd>
+              <p class="mod-link-text"><a href="/csr/fair_operating_practices/compliance_required_pharmaceuticals/index.html">「企業活動と医療機関等の関係の透明性に関する指針」ならびに「企業活動と患者団体の関係の透明性に関する指針」に基づいた2019年の企業活動情報を公開しました</a></p>
+            </dd>
+          </div>
+          <div class="list">
+            <dt>2020年9月11日</dt>
+            <dd>
+              <p class="mod-link-text"><a href="/csr/fair_operating_practices/compliance_required_pharmaceuticals/index.html">「企業活動と医療機関等の関係の透明性に関する指針」ならびに「企業活動と患者団体の関係の透明性に関する指針」に基づいた2019年の企業活動情報を公開しました</a></p>
+            </dd>
+          </div>
+          <div class="list">
+            <dt>2020年9月11日</dt>
+            <dd>
+              <p class="mod-link-text"><a href="/csr/fair_operating_practices/compliance_required_pharmaceuticals/index.html">「企業活動と医療機関等の関係の透明性に関する指針」ならびに「企業活動と患者団体の関係の透明性に関する指針」に基づいた2019年の企業活動情報を公開しました</a></p>
+            </dd>
           </div>
         </dl>
       </div>
@@ -48,6 +96,9 @@ export default {
 * {
   font-family: "Microsoft JhengHei", Arial, sans-serif;
   }
+.ContactUs {
+  height:100vh;
+}  
 .inner {
   z-index:-100;
   position: relative;
@@ -55,14 +106,19 @@ export default {
   background-color: #fef3ed;
 }
 .mod-heading-l1 {
+  text-align: center;
   max-width: 980px;
-  margin: 0;
+  top: 50%;
+  right: 50%;
+  width: 100%;
+  position: absolute;
   transform: translate(50%, 50%);
 }
 .title {
   border:#0000FF 5px solid;
   top: 50%;
   right: 50%;
+  
   width: 100%;
   font-size: 5rem;
   font-weight: bold;
@@ -71,5 +127,12 @@ export default {
 }
 .str-outer {
   margin-top: 100px;
+}
+.str-inner {
+  max-width: 1000px;
+  margin: 0 auto;
+}
+.mod-list-description {
+  text-align: left;
 }
 </style>
