6a2606a1dc795ed77a40a54f168439b70228e13e
黃兆湘 <ys9531425a@gmail.com>
2021-04-01 18:08:34 +0800

    Header

---
+++ src/components/Header.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="Header">
+      <div class="header_ row p-0 m-0">
+        <div class="col p-0 m-0 pl-5 d-flex justify-content-start align-items-center"> 
+          <img style="width:153px;height:26px" src="../assets/logo_header_01.png">
+        </div>
+        <div class="col row p-0 m-0 d-flex justify-content-end">
+          <div class="d-flex justify-content-center align-items-center" style="width:52px;"> 
+            <img style="width:26px;height:26px" src="../assets/icon_search_01.png">
+          </div>
+          <div class="d-flex justify-content-center align-items-center" style="width:52px;border-left: 1px solid #c9c9c9;"> 
+            <img style="width:26px;height:17px" src="../assets/icon_mail_01.png">
+          </div>
+          <div class="d-flex justify-content-center align-items-center" style="width:52px;border-left: 1px solid #c9c9c9;"> 
+            <img style="width:31px;height:36px" src="../assets/icon_global_01.png">
+          </div>
+        </div>
+      </div>
+      <!-- <font style="font-size:5rem !important">112334896</font> -->
+      <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.header_{
+    width: 100vw;
+    height:52px;
+    background: #FFFFFF 0% 0% no-repeat padding-box;
+    box-shadow: 0px 3px 6px #00000029;
+    opacity: 1;
+    position: fixed;
+}
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/Home'
 import ContactUs from '@/components/ContactUs'
-import HeaderAndFooter from '@/components/HeaderAndFooter'
+import Header from '@/components/Header'
 
 Vue.use(Router)
 
@@ -10,8 +10,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HeaderAndFooter',
-      component: HeaderAndFooter,
+      name: 'Header',
+      component: Header,
       children:[
         {
           path: '/Home',
